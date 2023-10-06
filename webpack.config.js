e724efdd7da405d2154b6ea454363a6baa8007f7
Divek Das <divekdas@gmail.com>
2023-10-06 19:05:45 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const opn = require('opn'); // Import the 'opn' package
+const opn = require("opn"); // Import the 'opn' package
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
- const publicPath = '/';
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const publicPath = "/";
 // const publicPath = '/react/';
 
 module.exports = {
@@ -24,7 +24,7 @@ module.exports = {
   // // },
   // // hot: "only",
   // //   hot: "only", // hot:true
-   
+
   // //  //  publicPath: '/', //relative path to output path where  devserver will look for compiled files
   // //   // host: '0.0.0.0',
   // //   //  compress: true,
@@ -43,21 +43,21 @@ module.exports = {
     },
   },
   externals: {
-   // global app config object
-   config: JSON.stringify({
-    apiUrl: '',
-    // publicPath : '/react/'  ,    
-    publicPath : '/',     
-   })
- },
- output: {
-  filename: 'js/[name].bundle.js',
-  path: path.resolve(__dirname, 'dist'), // base path where to send compiled assets
-  publicPath: publicPath, // base path where referenced files will be look for
-},
- resolve: {
-   extensions: ['.tsx', '.ts', '.js', '.jsx'],
- },
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: "",
+      // publicPath : '/react/'  ,
+      publicPath: "/",
+    }),
+  },
+  output: {
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist"), // base path where to send compiled assets
+    publicPath: publicPath, // base path where referenced files will be look for
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
   module: {
     rules: [
       {
@@ -66,58 +66,67 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
-    { // config for sass compilation
-      test: /\.scss$/,
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-        'css-loader',
-        {
-          loader: "sass-loader"
-        }
-      ]
-    },
-    {
-      test: /\.(jpg|png|svg|gif)$/,
-      type: 'asset/resource',
-    },
-    ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // config for sass compilation
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-  }),
-    new webpack.HotModuleReplacementPlugin(),
-    
-    new HtmlWebpackPlugin({
-      title: 'Hot Module Replacement',
-      template: "./public/index.html",
-        filename: "./index.html",
-        favicon: './public/favicon.png',
-   
-        // favicon: './public/favicon.png'
+      // 'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
-    new MiniCssExtractPlugin({ // plugin for controlling how compiled css will be outputted and named
-      filename: "css/[name].css",
-      chunkFilename: "css/[id].css"
-  }),
-  new CleanWebpackPlugin({
-    cleanOnceBeforeBuildPatterns: ["css/*.*", "js/*.*", "fonts/*.*", "images/*.*"]
-}),
+    new webpack.HotModuleReplacementPlugin(),
 
-  new webpack.ProvidePlugin({ //To automatically load jquery
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery'
-  })
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement",
+      template: "./public/index.html",
+      filename: "./index.html",
+      favicon: "./public/favicon.png",
+
+      // favicon: './public/favicon.png'
+    }),
+    new MiniCssExtractPlugin({
+      // plugin for controlling how compiled css will be outputted and named
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        "css/*.*",
+        "js/*.*",
+        "fonts/*.*",
+        "images/*.*",
+      ],
+    }),
+
+    new webpack.ProvidePlugin({
+      //To automatically load jquery
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
   ],
-}
+};
