034a55f389380892c7da732023d59f00680c2e4a
Divek Das <divekdas@gmail.com>
2023-10-06 22:29:06 +0530

    config updates

--- webpack.config.js
+++ webpack.config.js
@@ -96,7 +96,6 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("development"),
-      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
     new webpack.HotModuleReplacementPlugin(),
 
