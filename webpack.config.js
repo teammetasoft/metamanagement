c40ce5a2e89a8110149fa8f112b48b200ec6a9a3
Divek Das <divekdas@gmail.com>
2023-10-06 22:39:14 +0530

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -94,8 +94,11 @@ module.exports = {
     ],
   },
   plugins: [
+    // new webpack.DefinePlugin({
+    //   "process.env.NODE_ENV": JSON.stringify("development"),
+    // }),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development"),
+      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
     new webpack.HotModuleReplacementPlugin(),
 
